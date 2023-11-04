692310b7f6b11f49d00e42ce61e1c81a621ed9c1
JayRGopal <jay2003@me.com>
2023-11-04 18:59:32 -0400

    downloading 256

--- pytorch/download_pretrained.sh
+++ pytorch/download_pretrained.sh
@@ -5,4 +5,5 @@ wget -P assessors http://ganalyze.csail.mit.edu/models/EmoNet_valence_moments_re
 
 echo "Downloading BigGAN weights"
 wget -P generators http://ganalyze.csail.mit.edu/models/biggan-128.pth
+wget -P generators http://ganalyze.csail.mit.edu/models/biggan-256.pth
 
