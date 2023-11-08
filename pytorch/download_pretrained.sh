dc81d7bae2f3666b860ebcbc746ebf09d4f2db51
gaga1313 <63440697+gaga1313@users.noreply.github.com>
2023-11-07 23:50:58 -0500

    Update download_pretrained.sh

--- pytorch/download_pretrained.sh
+++ pytorch/download_pretrained.sh
@@ -2,10 +2,11 @@
 
 
 echo "Downloading EmoNet weights"
-gdown --output assessors 15LSbjI9TRiMp5jq2fRNj7DSSTkH-6Heu
+gdown --output 'assessors/EmoNet_valence_moments_resnet50_5_best.pth.tar' 15LSbjI9TRiMp5jq2fRNj7DSSTkH-6Heu
 
 echo "Downloading BigGAN weights"
-gdown --output generators 15Q0FfQNmpg-3Ubf43YQL5gBxr7ILT3VO
+gdown --output 'generators/biggan-256.pth' 15Q0FfQNmpg-3Ubf43YQL5gBxr7ILT3VO
+
 
 mkdir 'checkpoints'
-gdown --output checkpoints 1jknRrJSQ2SGlHqvSTFnLHKQsC4UhYFGr
+gdown --output 'checkpoints/pytorch_model_40000.pth' 1jknRrJSQ2SGlHqvSTFnLHKQsC4UhYFGr
